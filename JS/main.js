f695f259e2c1e1f29afaeb3fccaa3735efa71885
denial07 <108965893+denial07@users.noreply.github.com>
2024-09-02 10:41:23 +0800

    expense-inital

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    function addTask() {
+        var inputBox = document.getElementById("todo-input-box");
+        var listContainer = document.getElementById("todo-list-container");
+
+        if (inputBox.value === '') {
+            alert("You must write something!!!");
+        } else {
+            let li = document.createElement("li");
+            li.innerHTML = inputBox.value;
+            listContainer.appendChild(li);
+
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            span.classList.add("close");
+            li.appendChild(span); // Corrected line
+        }
+
+        inputBox.value = ''; // Clear the input box after adding the task
+    }
+
+    document.getElementById("add-task").addEventListener("click", addTask);
+
+    document.getElementById("todo-list-container").addEventListener("click", function(e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+        } 
+        else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+        }
+    }, false);
+};
